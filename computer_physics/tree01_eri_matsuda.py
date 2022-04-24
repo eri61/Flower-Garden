95f3a25415133be0fea7619405caf199fe260317
eri <wzkwdgm.rei@gamil.com>
2022-04-24 11:51:25 +0900

    add tree01_eri_matsuda.py

--- computer_physics/tree01_eri_matsuda.py
+++ computer_physics/tree01_eri_matsuda.py
@@ -1,8 +1,7 @@
 """
-Title: myname
----A program to print out "My name is Eri Matsuda." ---
+Title:Tree
 Author: Eri Matsuda
-Usafge: Run the program and the message appears.
+Usafge: Run the program and a tree appears.
 """
 
 # print out my name blow
