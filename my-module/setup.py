4e140abc2140e5731c9e735eeadf56bffc7878e5
eri <wzkwdgm.rei@gamil.com>
2022-05-05 21:11:21 +0900

    The self-made module(simple_peroperty) is completed

--- my-module/setup.py
+++ my-module/setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='simple_property',
-    version='0.1.0',
+    version='0.1.1',
     license='proprietary',
     packages=find_packages(),
 )
